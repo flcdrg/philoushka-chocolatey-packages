480f1910ebd8a4013f931efbbfaefb06e26cd28f
Phil Campbell <philoushka@gmail.com>
2015-09-22 11:04:49 -0700

    update for mirth 3.2.2

--- mirth/tools/chocolateyInstall.ps1
+++ mirth/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName='mirth'
 $fileType = 'exe'
-$url = 'http://downloads.mirthcorp.com/connect/3.2.2.7694.b68/mirthconnect-3.2.2.7694.b68-windows.exe'
+$url = 'http://downloads.mirthcorp.com/connect/3.3.0.7801.b1804/mirthconnect-3.3.0.7801.b1804-windows.exe'
 $silentArgs = '-q -console'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
