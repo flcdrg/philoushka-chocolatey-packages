d2c33ec62c0925131d5674291c479d702b866793
David Gardiner <david@gardiner.net.au>
2016-05-16 12:11:57 +0930

    Update for 3.4.0.8000

--- mirth/tools/chocolateyInstall.ps1
+++ mirth/tools/chocolateyInstall.ps1
@@ -1,6 +1,27 @@
-$packageName='mirth'
-$fileType = 'exe'
-$url = 'http://downloads.mirthcorp.com/connect/3.3.1.7856.b91/mirthconnect-3.3.1.7856.b91-windows.exe'
-$silentArgs = '-q -console'
+$ErrorActionPreference = 'Stop'; # stop on all errors
 
-Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
+$packageName='mirth'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://downloads.mirthcorp.com/connect/3.4.0.8000.b1959/mirthconnect-3.4.0.8000.b1959-windows.exe'
+$url64      = 'http://downloads.mirthcorp.com/connect/3.4.0.8000.b1959/mirthconnect-3.4.0.8000.b1959-windows-x64.exe'
+$silentArgs = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  #MSI
+  silentArgs    = "-q -console" 
+
+  # optional, highly recommended
+  softwareName  = 'Mirth Connect *' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = 'E2E12D5A6ECF68984C6FC0F2767F12CE'
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = '212E5BA1EBE48A1EEF8DA0384C5D2359'
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
